7ff3d25fa992bb79322af54037e82ccf686ca12b
romarios1987 <romios047@gmail.com>
2017-10-29 13:54:03 +0300

    third step (work app with zipcode us)

--- app.js
+++ app.js
@@ -1,19 +1,103 @@
+// Listen for submit
 document.querySelector('#zipForm').addEventListener
 ('submit', getLocationInfo);
 
+// Listen for delete
+document.querySelector('body').addEventListener
+('click', deleteLocation);
+
 
 function getLocationInfo(e) {
-
     // Get zip value from input
     const zip = document.querySelector('.zip').value;
 
-
-
     // Make request
     fetch(`http://api.zippopotam.us/us/${zip}`)
-        .then(response => response.json())
+        .then(response => {
+            if (response.status != 200) {
+                showIcon('remove');
+                document.querySelector('#output').innerHTML =
+                    `<article class="message is-danger"><div class="message-body">Invalid zip code. Please try again!</div></article>`;
+                throw Error(response.statusText);
+            } else {
+                showIcon('check');
+                return response.json();
+            }
+        })
         .then(date => {
-        console.log(date);
-    });
+            // Show location info
+            let output = '';
+            date.places.forEach(place => {
+                output += `
+                    <article class="message is-primary">
+                        <div class="message-header">
+                            <p>Location Info</p>
+                            <button class="delete"></button>
+                        </div>
+                        <div class="message-body">
+                            <ul>
+                                <li><strong>City:</strong> ${place['place name']}</li>
+                                <li><strong>State:</strong> ${place['state']}</li>
+                                <li><strong>Latitude:</strong> ${place['latitude']}</li>
+                                <li><strong>Longitude:</strong> ${place['longitude']}</li>
+                            </ul>
+                        </div>
+                    </article>`;
+            });
+            // Insert Into output div
+            document.querySelector('#output').innerHTML = output;
+            //console.log(date);
+        })
+        .catch(err => console.log(err));
     e.preventDefault();
 }
+
+// show check or remove icon
+function showIcon(icon) {
+    // Clear icons
+    document.querySelector('.icon-remove')
+        .style.display = "none";
+    document.querySelector('.icon-check')
+        .style.display = "none";
+
+    // Show correct icons
+    document.querySelector(`.icon-${icon}`)
+        .style.display = "inline-flex";
+}
+
+// delete location box
+function deleteLocation(e) {
+    if (e.target.className == 'delete') {
+        document.querySelector('.message').remove();
+        document.querySelector('.zip').value = '';
+        document.querySelector('.icon-check').remove();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
